e52bfef41486d5f1fc75193a1da55f1ad2dae518
Rahul Rudra <rahulrudra665@gmail.com>
2023-06-30 09:28:10 +0600

    changes commit

--- FrontEnd/reactprojects/tailwind.config.js
+++ FrontEnd/reactprojects/tailwind.config.js
@@ -4,5 +4,30 @@ module.exports = {
   theme: {
     extend: {}
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#176F6B",
+
+          secondary: "FFC000",
+
+          accent: "#FFEDD5",
+
+          neutral: "#F3F4F6",
+
+          "base-100": "#1d232a",
+
+          info: "#98A8DD",
+
+          success: "#1BBB70",
+
+          warning: "#DF7E07",
+
+          error: "#FA5C5C"
+        }
+      }
+    ]
+  },
   plugins: [require("daisyui")]
 };
